fd189073b048cd42364cb4acb67d3789f6d3cfe7
Feng <tf.wang.seu@gmail.com>
2026-09-16 15:02:27 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -6,10 +6,6 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -27,6 +23,13 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	con.Write([]byte("+PONG\r\n"))
-	con.Close()
+	buf := make([]byte, 1024)
+	for {
+		_, err := con.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading: ", err.Error())
+			os.Exit(1)
+		}
+		con.Write([]byte("+PONG\r\n"))
+	}
 }
