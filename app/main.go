c1e9b9b7f0787e9dc287de9f5d6f02a6ac8aa02a
Feng <tf.wang.seu@gmail.com>
2026-09-17 10:27:00 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"strconv"
 )
 
 var pong = []byte("+PONG\r\n")
@@ -46,8 +48,75 @@ func runEventLoop(events <-chan commandEvent) {
 	}
 }
 
-func execute(_ []byte) []byte {
-	return pong
+func execute(command []byte) []byte {
+	arguments, err := parseRESPCommand(command)
+	if err != nil || len(arguments) == 0 {
+		return []byte("-ERR protocol error\r\n")
+	}
+
+	switch {
+	case bytes.EqualFold(arguments[0], []byte("PING")) && len(arguments) == 1:
+		return pong
+	case bytes.EqualFold(arguments[0], []byte("ECHO")) && len(arguments) == 2:
+		return bulkString(arguments[1])
+	case bytes.EqualFold(arguments[0], []byte("ECHO")):
+		return []byte("-ERR wrong number of arguments for 'echo' command\r\n")
+	default:
+		return []byte("-ERR unknown command\r\n")
+	}
+}
+
+func bulkString(value []byte) []byte {
+	response := make([]byte, 0, len(value)+32)
+	response = append(response, '$')
+	response = strconv.AppendInt(response, int64(len(value)), 10)
+	response = append(response, '\r', '\n')
+	response = append(response, value...)
+	response = append(response, '\r', '\n')
+	return response
+}
+
+func parseRESPCommand(command []byte) ([][]byte, error) {
+	if len(command) < 4 || command[0] != '*' {
+		return nil, errors.New("command is not a RESP array")
+	}
+
+	lineEnd := bytes.Index(command, []byte("\r\n"))
+	if lineEnd == -1 {
+		return nil, errors.New("incomplete RESP array header")
+	}
+	count, err := strconv.Atoi(string(command[1:lineEnd]))
+	if err != nil || count < 1 {
+		return nil, errors.New("invalid RESP array length")
+	}
+
+	arguments := make([][]byte, 0, count)
+	position := lineEnd + 2
+	for range count {
+		if position >= len(command) || command[position] != '$' {
+			return nil, errors.New("command argument is not a RESP bulk string")
+		}
+
+		argumentEnd := bytes.Index(command[position:], []byte("\r\n"))
+		if argumentEnd == -1 {
+			return nil, errors.New("incomplete RESP bulk string header")
+		}
+		argumentEnd += position
+
+		length, err := strconv.Atoi(string(command[position+1 : argumentEnd]))
+		if err != nil || length < 0 {
+			return nil, errors.New("invalid RESP bulk string length")
+		}
+		position = argumentEnd + 2
+		if position+length+2 > len(command) || !bytes.Equal(command[position+length:position+length+2], []byte("\r\n")) {
+			return nil, errors.New("incomplete RESP bulk string")
+		}
+
+		arguments = append(arguments, command[position:position+length])
+		position += length + 2
+	}
+
+	return arguments, nil
 }
 
 func handleConn(conn net.Conn, events chan<- commandEvent) {
